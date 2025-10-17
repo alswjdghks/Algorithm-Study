0f12d13c759e06120e102ff14fce39c29df0ff00
jerrymin1221 <jerrymin1221@naver.com>
2025-10-18 04:30:13 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,19 @@
+SELECT
+    ITEM_ID,
+    ITEM_NAME,
+    RARITY
+FROM
+    ITEM_INFO
+WHERE
+    ITEM_ID IN (SELECT
+                    t.ITEM_ID
+                FROM
+                    ITEM_INFO i
+                JOIN
+                    ITEM_TREE t ON i.ITEM_ID = t.PARENT_ITEM_ID 
+                WHERE
+                    i.RARITY = 'RARE')
+ORDER BY
+    ITEM_ID DESC;
+
+    
