25ce300efc50ac117d586d5a86aa2de9bfb5507a
jerrymin1221 <jerrymin1221@naver.com>
2025-11-14 18:15:36 +0900

    [D2] Title: 파리 퇴치, Time: 89 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -0,0 +1,71 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
+import java.io.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int[][] arr = new int[N][N];
+            int max = 0 ;
+            
+			for(int i = 0 ; i < N ; i++){
+                st = new StringTokenizer(br.readLine());
+            	for(int j = 0 ; j < N ; j++){
+                	arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            
+            for(int k = 0 ; k <= N-M ; k++){
+            	for(int z = 0 ; z <= N-M ; z++){
+                     int sum = 0;
+                	for(int x = k ; x < k+M ; x++){
+                    	for(int y = z ; y < z+M ; y++){
+                        	sum += arr[x][y];
+                        }
+                    }
+                    if( sum > max) max = sum;
+                }
+            }
+            System.out.println("#"+test_case+" "+max);
+		}
+	}
+}
