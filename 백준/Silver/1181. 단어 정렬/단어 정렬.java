d517cf607464f96167558a9007b4b7a191eb387a
jerrymin1221 <jerrymin1221@naver.com>
2025-11-24 19:58:17 +0900

    [Silver V] Title: 단어 정렬, Time: 496 ms, Memory: 24256 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 13
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                if(s1.length() == s2.length()){
+                    return s1.compareTo(s2);
+                }
+                return s1.length() - s2.length();
+            }
+        });
+
+        System.out.println(arr[0]);
+
+        for(int i = 1; i <= N-1 ; i++){
+            if(!arr[i].equals(arr[i-1])){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
