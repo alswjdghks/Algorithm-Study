90ef9c30d74a2b805a5f3d3813ee0763440950d9
jerrymin1221 <jerrymin1221@naver.com>
2026-01-29 11:57:17 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 280 ms, Memory: 20536 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -3,25 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int count = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        for (int i = 0; i < N; i++) {
-            String str = bf.readLine();
-            System.out.println(isPalindrome(str) + " " +count);
-            count = 0;
-        }
-    }
-
-    public static int recursion(String s, int start, int end){
-        count++;
-        if (start >= end) return 1;
-        else if (s.charAt(start) != s.charAt(end)) return 0;
-        else return recursion(s, start + 1, end-1);
-    }
-
-    public static int isPalindrome(String s){
-        return recursion(s, 0, s.length()-1);
-    }
+	
+	public static int count;
+	
+	public static void main(String[] args)throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < T ; i++) {
+			count = 0;
+			String str = br.readLine();
+			System.out.println(isPalindrome(str,0,str.length()-1) + " "  + count);
+		}
+	}
+	
+	private static int isPalindrome(String s, int first, int last) {
+		count++;
+		if(first >= last) {
+			return 1;
+		}else if(s.charAt(first) != s.charAt(last)) {
+			return 0;
+		}else {
+			return isPalindrome(s,first+1, last-1);
+		}
+	}
 }
