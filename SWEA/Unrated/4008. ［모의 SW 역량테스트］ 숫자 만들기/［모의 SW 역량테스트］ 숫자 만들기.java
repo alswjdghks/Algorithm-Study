8b4bd5550105bfa481cdbe7ccce14e2c09931b4a
jerrymin1221 <jerrymin1221@naver.com>
2026-02-16 15:05:46 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 107 ms, Memory: 27,512 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -4,64 +4,70 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+    static int[] oper;
+    static int[] numbers;
+    static int N;
+    static int M;
 
-	static int[] numbers;
-	static int[] oper;
-	static int N;
-	static int max;
-	static int min;
+    static int max;
+    static int min;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int TC = Integer.parseInt(br.readLine()); // 10
+        for (int tc = 1 ; tc <= TC ; tc++) {
+            max = Integer.MIN_VALUE;
+            min = Integer.MAX_VALUE;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int TC = Integer.parseInt(br.readLine()); // 10
+            N = Integer.parseInt(br.readLine()); // 5
+            M = 0;
+            numbers = new int[N];
 
-		for (int tc = 1; tc <= TC; tc++) {
-			N = Integer.parseInt(br.readLine()); // 5
-			numbers = new int[N];
-			oper = new int[4];
+            oper = new int[4];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < 4; i++) {
+                oper[i] = Integer.parseInt(st.nextToken());
+                M += oper[i];
+            }
 
-			max = Integer.MIN_VALUE;
-			min = Integer.MAX_VALUE;
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                numbers[i] = Integer.parseInt(st.nextToken());
+            }
 
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 4; i++) {
-				oper[i] = Integer.parseInt(st.nextToken());
-			}
+            dfs(0,numbers[0]);
+            sb.append("#").append(tc).append(" ").append(max - min).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < N; i++) {
-				numbers[i] = Integer.parseInt(st.nextToken());
-			}
+    private static void dfs(int depth, int total){
+        if (depth == M){
+            max = Math.max(max, total);
+            min = Math.min(min, total);
+            return;
+        }
 
-			backTracking(0, numbers[0], oper[0], oper[1], oper[2], oper[3]);
-
-			System.out.println("#" + tc + " " + (max - min));
-		}
-	}
-
-	private static void backTracking(int idx, int prev, int plus, int minus, int multiple, int division) {
-		if (idx == N - 1) {
-			max = Math.max(max, prev);
-			min = Math.min(min, prev);
-			return;
-		}
-
-		if (plus > 0) {
-			backTracking(idx + 1, prev + numbers[idx+1], plus - 1, minus, multiple, division);
-		}
-
-		if (minus > 0) {
-			backTracking(idx + 1, prev - numbers[idx+1], plus, minus - 1, multiple, division);
-		}
-
-		if (multiple > 0) {
-			backTracking(idx + 1, prev * numbers[idx+1], plus, minus, multiple - 1, division);
-		}
-
-		if (division > 0) {
-			backTracking(idx + 1, prev / numbers[idx+1], plus, minus, multiple, division - 1);
-		}
-	}
-
+        if (oper[0] > 0){
+            oper[0]--;
+            dfs(depth+1,total + numbers[depth+1]);
+            oper[0]++;
+        }
+        if (oper[1] > 0){
+            oper[1]--;
+            dfs(depth+1,total - numbers[depth+1]);
+            oper[1]++;
+        }
+        if (oper[2] > 0){
+            oper[2]--;
+            dfs(depth+1,total * numbers[depth+1]);
+            oper[2]++;
+        }
+        if (oper[3] > 0){
+            oper[3]--;
+            dfs(depth+1,total / numbers[depth+1]);
+            oper[3]++;
+        }
+    }
 }
