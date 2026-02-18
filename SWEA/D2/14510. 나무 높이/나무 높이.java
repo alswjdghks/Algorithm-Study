d97cf840eec1b803a8724f6ea2480ac618ab2703
jerrymin1221 <jerrymin1221@naver.com>
2026-02-18 23:39:06 +0900

    [D2] Title: 나무 높이, Time: 95 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -4,53 +4,51 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] trees;
-	static int[] height;
-	static int min;
+    static StringBuilder sb = new StringBuilder();
+    static int[] trees;
+    static int[] diff;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int TC = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= TC; tc++) {
-			int N = Integer.parseInt(br.readLine()); // 2
-			trees = new int[N];
-			height = new int[N];
-			min = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int TC = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= TC; i++) {
+            sb.append("#").append(i).append(" ");
+            int N = Integer.parseInt(br.readLine());
+            trees = new int[N];
+            diff = new int[N];
 
-			StringTokenizer st = new StringTokenizer(br.readLine());
+            int max = Integer.MIN_VALUE;
+            int sum = 0;
 
-			int max = Integer.MIN_VALUE;
-			for (int i = 0; i < N; i++) {
-				trees[i] = Integer.parseInt(st.nextToken());
-				max = Math.max(max, trees[i]);
-			}
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                trees[j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, trees[j]);
+            }
 
-			int odd = 0;
-			int even = 0;
+            int odd = 0; // 홀수
+            int even = 0; // 짝수
 
-			// 모든 나무에 대해 필요한 1과 2의 개수 총합하기
-			for (int i = 0; i < N; i++) {
-				height[i] = max - trees[i];
-				even += height[i] / 2;
-				odd += height[i] % 2;
-			}
+            for (int j = 0; j < N; j++) {
+                diff[j] = max - trees[j];
+                odd += diff[j] % 2;
+                even += diff[j] / 2;
+            }
 
-			// 2를 1 2개로 쪼개기
-			while (even - odd > 1) {
-				even--;
-				odd += 2;
-			}
+            while ( even - odd > 1){
+                --even;
+                odd += 2;
+            }
 
-			if (even > odd) {
-				min = 2 * even;
-			} else if (odd > even) {
-				min = 2 * odd - 1;
-			} else {
-				min = 2 * even;
-			}
-
-			System.out.println("#" + tc + " " + min);
-
-		}
-	}
+            if (even == odd){
+                sb.append(2*even).append("\n");
+            }else if(even > odd){
+                sb.append(2*even).append("\n");
+            }else{
+                sb.append(2*odd-1).append("\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
