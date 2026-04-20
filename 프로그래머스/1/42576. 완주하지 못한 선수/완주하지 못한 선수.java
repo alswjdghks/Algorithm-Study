15574699e0ae27ac757c7c6ba3e92dfd86d8de0e
jerrymin1221 <jerrymin1221@naver.com>
2026-04-20 16:39:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.11 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String name : participant){
+            if(!map.containsKey(name)){
+                map.put(name,1);
+            }else{
+                map.put(name, map.get(name)+1);
+            }
+            
+        }
+        for(String name : completion){
+            map.put(name, map.get(name)-1);
+        }
+        
+        for(String name : participant){
+            if(map.get(name) == 1){
+                answer = name;
+            }
+        }
+        
+        return answer;
+    }
+}
